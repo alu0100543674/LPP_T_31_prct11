07f8d0ed37f619c4c7cbc25c9937cb9e21eb583e
alu0100543674 <alu0100543674@ull.edu.es>
2014-11-13 10:52:38 +0000

    Clase lista.rb terminada y comentada

--- lib/examen/lista.rb
+++ lib/examen/lista.rb
@@ -1,6 +1,6 @@
 module QuestionExamen
   
-	# Estructura de un nodo
+	# Estructura de un nodo (valor, siguiente, anterior)
   	Node = Struct.new(:value, :next , :anterior) 
   
 	# Clase Pregunta
@@ -35,126 +35,124 @@ module QuestionExamen
     			@tail = nil
 		end
 
-    		# Añadir un nodo
-     		def push(nodo)
+    	# Añadir un nodo
+     	def push(nodo)
 			# Si cabeza no apunta a ningun nodo
        			if @cabeza == nil
 	 			@cabeza = nodo
 	 			@tail = nodo
 			# Si cabeza apunta a un nodo
        			else 
-				nodo.next = @cabeza
-				@cabeza.anterior = nodo
-				@cabeza = nodo
-				nodo.anterior = nil
-	
-       			end
+					nodo.next = @cabeza
+					@cabeza.anterior = nodo
+					@cabeza = nodo
+					nodo.anterior = nil
+	   			end
    		end
         
 		# Extraer 
-      		def pop()
+    	def pop()
 			# Si hay mas de un nodo en la lista
 	  		if (@cabeza != @tail)
 	   			aux = @cabeza
-	    			@cabeza = @cabeza.next
-	    			@cabeza.anterior = nil
-	    			aux.next = nil
-	    			aux.value
+	   			@cabeza = @cabeza.next
+	   			@cabeza.anterior = nil
+	   			aux.next = nil
+	   			aux.value
 			# Si solo hay un nodo en la lista
 	  		else
-	    			@cabeza = nil
-	    			@tail = nil
-	    
+	   			@cabeza = nil
+	   			@tail = nil
 	  		end
-     		end
+    	end
      
 		# Añadimos un nodo por el final
-     		def push_final(nodo)
+    	def push_final(nodo)
 			# Si tail no apunta a ningun nodo
-       			if @tail == nil
+       		if @tail == nil
 	 			@tail = nodo
 	 			@cabeza = nodo
 			# Si tail apunta a un nodo
-       			else
+       		else
 	  			@tail.next = nodo
 	  			nodo.anterior = @tail
 	  			@tail = nodo
-       			end
-    		end
+       		end
+    	end
      
 		# Extraemos por el final
-     		def pop_final()
+     	def pop_final()
 			# Si hay mas de un nodo en la lista
-       			if (@cabeza != @tail)
-	    			aux = @tail
-	    			@tail = @tail.anterior
-	    			@tail.next = nil
-	    			aux.anterior = nil
-	    			aux.value
-				# Si solo hay un nodo en la lista
-	  			else
-	    			@cabeza = nil
-	    			@tail = nil
-	  			end
-     		end
+       		if (@cabeza != @tail)
+	    		aux = @tail
+	    		@tail = @tail.anterior
+	    		@tail.next = nil
+	    		aux.anterior = nil
+	    		aux.value
+			# Si solo hay un nodo en la lista
+	  		else
+	    		@cabeza = nil
+	    		@tail = nil
+	  		end
+     	end
      		
-          	# Metodo each
-     		def each 
-     			aux = @tail
-     			while aux != nil
-     				yield aux.value
-     				aux = aux.next
-     			end
-     	   	end
+        # Metodo each
+     	def each 
+     		aux = @tail
+     		while aux != nil
+     			yield aux.value
+     			aux = aux.next
+     		end
+     	end
 end
   
   	# Clase SeleccionSimple que hereda de Pregunta
   	class SeleccionSimple < Pregunta
     
-    		attr_accessor :right, :distractor
+    	attr_accessor :right, :distractor
     
 		# Initialize
-    		def initialize(question, right, distractor, nivel)
-      			super(question, nivel)      
-      			@right, @distractor = right, distractor
-      		end
+    	def initialize(question, right, distractor, nivel)
+      		super(question, nivel)      
+      		@right, @distractor = right, distractor
+      	end
     
-    		# Mostrar por pantalla
-    		def to_s
-      			opcion = @distractor+[@right]
-      			s= ' '
-     			opcion.each do |opcion|
+    	# Mostrar por pantalla
+    	def to_s
+      		opcion = @distractor+[@right]
+      		s= ' '
+     		opcion.each do |opcion|
 	
 			s += %Q{#{opcion}\n}
 	
-      			end
+      		end
 	 		"#{@question}\n#{s}\n"
-    		end  
+    	end  
     
  	end
   
 	# Clase VerdaderoFalso que hereda de Pregunta
 	class VerdaderoFalso < Pregunta
     
-    		attr_accessor :right, :wrong
+    	attr_accessor :right, :wrong
     
 		# Initialize
-    		def initialize(question, right, wrong, nivel)
-     			super(question, nivel)      
-      			@right, @wrong = right, wrong
-      		end
+    	def initialize(question, right, wrong, nivel)
+     		super(question, nivel)      
+      		@right, @wrong = right, wrong
+      	end
     
-    		# Mostrar por pantalla
-    		def to_s
-      			opcion = @right+[@wrong]
-      			s= ' '
-      			opcion.each do |opcion|
+    	# Mostrar por pantalla
+    	def to_s
+      		opcion = @right+[@wrong]
+      		s= ' '
+      		opcion.each do |opcion|
 	
 			s += %Q{#{opcion}\n}
 	
-      			end
+      		end
 	 		"#{@question}\n#{s}\n"
-    		end  
+    	end  
     
   	end 
 end
