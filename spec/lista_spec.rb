355453b64e6c4b48db1be1fd3ae87b50ccd95b4a
Samer Al Khindi <alu0100454888@ull.edu.es>
2014-11-13 16:23:24 +0000

    .rspec inicial subido , segundo intento

--- spec/lista_spec.rb
+++ spec/lista_spec.rb
@@ -1,2 +1,23 @@
+require 'spec_helper'
+require "./lib/examen/lista.rb"
 
+
+module QuestionExamen
+    
+    # Expectativas de la clase Pregunta
+    describe QuestionExamen::Pregunta do	  
+	  before :each do
+	    @pregunta1 = QuestionExamen::Pregunta.new('3 + 7 =', 0)
+	  end	  
+	    
+	  context " #Clase Preguntas" do
+	    it " # Debe tener un campo para la pregunta" do
+	      expect(@pregunta1.question) == '3 + 7 ='
+	    end
+	    it " #Debe tener un campo para el nivel de dificultad" do
+	      expect(@pregunta1.nivel) == 0
+	    end
+	  end	    
+    end
+    
  
