d807aa77818140f92cfce0b3d00dbe0d92bfa706
alu0100543674 <alu0100543674@ull.edu.es>
2014-11-28 20:19:36 +0000

    Practica 10

--- lib/examen/lista.rb
+++ lib/examen/lista.rb
@@ -1,226 +1,236 @@
-module QuestionExamen
+module Test
+    # Estructura de un nodo
+    Node = Struct.new(:value, :next , :anterior) #define una estructura indicando los campos que contendrá
   
-	# Estructura de un nodo (valor, siguiente, anterior)
-  	Node = Struct.new(:value, :next , :anterior) 
+    # Clase Pregunta
+    class Pregunta
+    
+        include Comparable
+    
+        attr_accessor :quest
+    
+        def initialize(quest)
+            @quest = quest
+        end
+    end
   
-	# Clase Pregunta
-	class Pregunta
+    # Clase Lista
+    class ListaEnlazada
     
-    		include Comparable
+        include Enumerable
     
-    		attr_accessor :question, :nivel
-    		
-    		
-    		def initialize(question, nivel)
-      			@question = question
-      			@nivel = nivel
-      		end
+        attr_accessor :cabeza, :tail
     
-    		def <=> (other)
-        		@nivel <=> other.nivel
-    		end
-    		
-    		def == (other)
-				@nivel == other.nivel && @question == other.question
-			end
-	end 
-	
-	
-	# Clase List
-	class List
+        def initialize()
+            @cabeza = nil
+            @tail = nil
+        end
+  
+        def each
+            nodo = @cabeza
+            while (nodo) do
+	            yield nodo.value
+                nodo = nodo.next
+            end
+        end
     
-    	include Enumerable
-    	
-		attr_accessor :cabeza, :tail
-     
-		# Initialize
-		def initialize()
-    			@cabeza = nil
-    			@tail = nil
-		end
-
-    	# Añadir un nodo
-     	def push(nodo)
-			# Si cabeza no apunta a ningun nodo
-       			if @cabeza == nil
-	 			@cabeza = nodo
-	 			@tail = nodo
-			# Si cabeza apunta a un nodo
-       			else 
-					nodo.next = @cabeza
-					@cabeza.anterior = nodo
-					@cabeza = nodo
-					nodo.anterior = nil
-	   			end
-   		end
+        def reverse_each
+            nodo=@tail
+            while (nodo) do
+	            yield nodo
+    	        nodo=nodo.anterior
+            end
+        end
+    
+        def top
+            @cabeza
+        end
+    
+        def push(nodo)
+            if @cabeza == nil
+	            @cabeza = nodo
+	            @tail = nodo
+            else 
+	            nodo.next = @cabeza
+        	    @cabeza.anterior = nodo
+        	    @cabeza = nodo
+            	nodo.anterior = nil
+            end
+        end
         
-		# Extraer 
-    	def pop()
-			# Si hay mas de un nodo en la lista
-	  		if (@cabeza != @tail)
-	   			aux = @cabeza
-	   			@cabeza = @cabeza.next
-	   			@cabeza.anterior = nil
-	   			aux.next = nil
-	   			aux.value
-			# Si solo hay un nodo en la lista
-	  		else
-	   			@cabeza = nil
-	   			@tail = nil
-	  		end
-    	end
+        def pop()
+	        if (@cabeza != @tail)#si la lista tiene mas de un elemento
+	            aux = @cabeza
+	            @cabeza = @cabeza.next
+	            @cabeza.anterior = nil
+	            aux.next = nil
+	            aux.value
+	        else
+	            @cabeza = nil
+	            @tail = nil
+	        end
+        end
      
-		# Añadimos un nodo por el final
-    	def push_final(nodo)
-			# Si tail no apunta a ningun nodo
-       		if @tail == nil
-	 			@tail = nodo
-	 			@cabeza = nodo
-			# Si tail apunta a un nodo
-       		else
-	  			@tail.next = nodo
-	  			nodo.anterior = @tail
-	  			@tail = nodo
-       		end
-    	end
+        def push_final(nodo)
+            if @tail == nil
+	            @tail = nodo
+	            @cabeza = nodo
+            else
+	            @tail.next = nodo
+	            nodo.anterior = @tail
+	            @tail = nodo
+            end
+        end
      
-		# Extraemos por el final
-     	def pop_final()
-			# Si hay mas de un nodo en la lista
-       		if (@cabeza != @tail)
-	    		aux = @tail
-	    		@tail = @tail.anterior
-	    		@tail.next = nil
-	    		aux.anterior = nil
-	    		aux.value
-			# Si solo hay un nodo en la lista
-	  		else
-	    		@cabeza = nil
-	    		@tail = nil
-	  		end
-     	end
-     		
-        # Metodo each
-     	def each 
-     		nodo = @head
-     		while (nodo != @tail) do
-     			yield nodo.value
-     			nodo = nodo.next
-     		end
-     	end
+        def pop_final()
+            if (@cabeza != @tail)#si la lista tiene mas de un elemento
+	            aux = @tail
+	            @tail = @tail.anterior
+	            @tail.next = nil
+	            aux.anterior = nil
+	            aux.value
+	        else
+	            @cabeza = nil
+	            @tail = nil
+	        end
+        end
      
-     	def add_many(values)
-     		values.each do |x|
-     			add(x)
-     		end
-     	end
-     	
-end
+        def cont_nodos
+            cont = 0
+            n=@cabeza
+            while (n != nil)do
+	            cont += 1
+	            n=n.next
+            end
+            return cont
+        end
+    end
   
-  	# Clase SeleccionSimple que hereda de Pregunta
-  	class SeleccionSimple < Pregunta
+    # Clase Simple que hereda de la clase Pregunta
+    class Simple < Pregunta
     
-    	attr_accessor :right, :distractor
+        attr_accessor :right, :distractor
     
-		# Initialize
-    	def initialize(question, right, distractor, nivel)
-      		super(question, nivel)      
-      		@right, @distractor = right, distractor
-      	end
+        def initialize(quest, right, distractor)
+            super(quest)
+            @right, @distractor = right, distractor
+        end
     
-    	# Mostrar por pantalla
-    	def to_s
-      		opcion = @distractor+[@right]
-      		s= ' '
-     		opcion.each do |opcion|
-	
-			s += %Q{#{opcion}\n}
-	
-      		end
-	 		"#{@question}\n#{s}\n"
-    	end  
-    
- 	end
+        def <=>(other)
+            distractor.size <=> other.distractor.size
+        end
+
+        def to_s
+            op = @distractor + [@right]
+            s = ' '
+            op.each do |op|
+	            s += %Q{#{op}\n}	
+            end
+            "#{@quest}\n#{s}\n"
+        end  
+    end 
   
-	# Clase VerdaderoFalso que hereda de Pregunta
-	class VerdaderoFalso < Pregunta
+  
+    # Clase TrueFalse que hereda de la clase Pregunta
+    class TrueFalse < Pregunta
     
-    	attr_accessor :right, :wrong
+        attr_accessor :right, :wrong
     
-		# Initialize
-    	def initialize(question, right, wrong, nivel)
-     		super(question, nivel)      
-      		@right, @wrong = right, wrong
-      	end
+        def initialize(quest, right, wrong)
+          super(quest)
+          @right, @wrong = right, wrong
+        end
     
-    	# Mostrar por pantalla
-    	def to_s
-      		opcion = @right+[@wrong]
-      		s= ' '
-      		opcion.each do |opcion|
-	
-			s += %Q{#{opcion}\n}
-	
-      		end
-	 		"#{@question}\n#{s}\n"
-    	end  
+        def <=>(other)
+           right.size <=> other.right.size
+        end
     
-  	end 
-  	
-	class Examen
-		attr_accessor :preguntas
-		def initialize(listaPreguntas)
-			@npreguntas = listaPreguntas
-		end
-		
-		def insertQuestion(question)
-			nodo = Nodo.new(question)
-			nodoInsertado = @preguntas.push(nodo)
-			nodoInsertado.value
-		end
-		
-		def to_s
-			aux = @preguntas.cabeza
-			respuesta = ""
-			respuesta += aux.value.to_s
-			respuesta += "\n"
-			while (aux != @preguntas.tail) do
-				aux = aux.next
-				respuesta += aux.value.to_s
-				respuesta += "\n"
-			end
-			respuesta
-		end
-	end
- 		
-	class Interface
-		attr_reader :examen
-		def initialize(examen)
-			@examen = examen
-		end
-		
-		def compresp(resp)
-			ok = 0
-			index = 0
-			num = 0
-			while n < 5 do
-				if @examen[i] == resp[i]
-					ok += 1
-				end
-				index += 1
-				num += 1
-			end
-			
-			if ok < (index/2)+1
-				p = "Suspendido: #{ok}/#{i}"
-			else
-				if ok == index
-					p = "Nota: 10"
-				else
-					p = "Aprobado: #{correctas}/#{i}"
-				end
-			end
-			p
-		end
-	end
-end
+        def to_s
+            op = @wrong + [@right]
+            s = ' '
+            op.each do |op|
+	            s += %Q{#{op}\n}
+	        end
+	        "#{@quest}\n#{s}\n"
+        end  
+    
+        def respuesta_correcta (eleccion)
+	        if (eleccion == @right)
+	            return true
+	        else
+	            return false
+	        end 
+        end
+    end 
+  
+    # Clase Interfaz
+    class Interfaz
+      
+        attr_accessor :contador_acertadas , :cont_erroneas, :arr_respuesta
+    
+        def initialize(arr_respuesta)
+	        @arr_respuesta= arr_respuesta
+	        @contador_acertadas= 0
+	        @cont_erroneas = 0
+        end
+      
+        def hacer_examen(respuestas)
+	        cont = 0
+	        num_preguntas = 0
+	        while num_preguntas <  5
+	            if (@arr_respuesta[cont] == respuestas[cont])
+		            @contador_acertadas = @contador_acertadas + 1
+	            else
+		            @cont_erroneas = @cont_erroneas + 1
+	            end
+	            cont = cont + 1
+	            num_preguntas += 1
+            end
+
+	        if (@contador_acertadas > @cont_erroneas)
+	            return true
+	        else
+	            return false
+	        end       
+        end
+    end
+  
+    # Clase Examen
+    class Examen
+    
+        include Enumerable
+ 
+        attr_accessor :lista
+      
+        def initialize(lista)
+	        @lista = lista
+        end
+      
+        def to_s
+            @lista.each do |q|
+                puts "#{q.to_s}\n"     
+            end
+        end
+      
+        def invertir(lista)
+	        @lista_invertida = Test::ListaEnlazada.new()
+	        lista.reverse_each do |q|
+	            @lista_invertida.push(q)
+	        end
+	        return @lista_invertida
+        end
+      
+        def invertir_funcional(lista)
+            ar = Array.new
+	        ar.push(lista)
+	        invertir = -> { reverse }
+	        ar = lista.instance_exec(&invertir)
+	        return ar
+        end
+    end
+end 
+
+
+
+  
+
