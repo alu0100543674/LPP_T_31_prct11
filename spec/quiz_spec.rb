bb8445b382ef1c1d653302f13a5058605ed917a7
Samer Al Khindi <alu0100454888@ull.edu.es>
2014-12-12 13:44:31 +0000

    Creando expectativas

--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -2,5 +2,19 @@ require 'spec_helper'
 require "./lib/examen/quiz.rb"
 
 module DSL
-
+	describe DSL::QuizZ do
+    		before :each do
+			@q = DSL::QuizZ.new("Cuestionario de LPP 05/12/2014") {
+				question '¿Cuantos argumentos de tipo bloque puede recibir un metodo?',
+	    			right => '1',
+	    			wrong => '2',
+	    			wrong => 'muchos',
+	  			wrong => 'los que defina el usuario'
+	  				
+	  			# question "En Ruby los bloques son objetos que contienen codigo",
+	  			# wrong => 'Cierto',
+	  			# right => 'Falso'
+			}
+  		end
+	end
 end
---
+++ spec/quiz_spec.rb~
@@ -0,0 +1,6 @@
+require 'spec_helper'
+require "./lib/examen/quiz.rb"
+
+module DSL
+
+end
