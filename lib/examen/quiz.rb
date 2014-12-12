de8d0af82baaa346aff1b594d5ea81896ec384ea
alu0100543674 <alu0100543674@ull.edu.es>
2014-12-12 13:42:49 +0000

    Comienzo clase QuizZ

--- lib/examen/quiz.rb
+++ lib/examen/quiz.rb
@@ -9,7 +9,20 @@ module DSL
     	def initialize (title, op_ans)
         	@title, @op_ans = title, op_ans
     	end
-
-    
 	end
+	
+	# Clase QuizZ
+	class QuizZ 
+  
+  		RIGHT = :right
+  		WRONG = :wrong
+  
+  		attr_accessor :name, :preg
+
+		# Initialize
+  		def initialize(name, &block)
+    		@name, @preg = name, []
+		end
+	end
+	
 end
